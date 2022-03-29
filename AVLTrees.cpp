29931306f24950217fb79e84a0630e321b53efaf
sabircodes <smdakhtar007@gmail.com>
2022-03-30 00:32:37 +0530

    delete function added

--- AVLTrees.cpp
+++ AVLTrees.cpp
@@ -104,47 +104,129 @@ node *insert(node *root, int val)
     {
         return rightrotation(root);
     }
-        if (balance < -1 && val > root->right->data)
+    if (balance < -1 && val > root->right->data)
+    {
+        return leftrotation(root);
+    }
+
+    if (balance > 1 && val > root->left->data)
+    {
+        root->left = leftrotation(root->left);
+        return rightrotation(root);
+    }
+
+    // Right Left Case
+    if (balance < -1 && val < root->right->data)
+    {
+        root->right = rightrotation(root->right);
+        return leftrotation(root);
+    }
+
+    /* return the (unchanged) node pointer */
+    return root;
+}
+
+node *minnode(node *&root)
+{
+    node *curr = root;
+    if (curr->left)
+    {
+        curr = curr->left;
+    }
+    return curr;
+}
+
+node *deleteNode(node *root, int val)
+{
+    if (root == NULL)
+    {
+        return root;
+    }
+    if (val < root->data)
+    {
+        root->left = deleteNode(root->left, val);
+    }
+    else if (val > root->right->data)
+    {
+        root->right = deleteNode(root->right, val);
+    }
+    else
+    {
+        if (root->left == NULL || root->right == NULL)
         {
-            return leftrotation(root);
+            delete root;
+            root = NULL;
         }
 
-        if (balance > 1 && val > root->left->data)
+        // case of 1 child
+        else if (root->right == NULL)
+        {
+            node *temp = root;
+            root = root->left;
+            delete temp;
+        }
+        else if (root->left == NULL)
+        {
+            node *temp = root;
+            root = root->right;
+            delete temp;
+        }
+
+        // case of 2 child nodes
+        else
+        {
+            node *temp = minnode(root->right);
+            root->data = temp->data;
+            root->right = deleteNode(root->right, temp->data);
+        }
+    }
+        if (root==NULL){return root;}
+
+        root ->height = max(height(root->left), height(root->right)) + 1;
+
+        int balance = getbalance(root);
+        // Left left case
+        if (balance > 1 && getbalance(root->left) >= 0)
+        {
+            return rightrotation(root);
+        }
+
+        // left right case
+        if (balance > 1 && getbalance(root->left) < 0)
         {
             root->left = leftrotation(root->left);
             return rightrotation(root);
         }
 
-        // Right Left Case
-        if (balance < -1 && val < root->right->data)
+        if (balance < -1 && getbalance(root->right) <= 0)
         {
-            root->right = rightrotation(root->right);
             return leftrotation(root);
         }
-
-        /* return the (unchanged) node pointer */
-        return root;
-    }
-
-void preorder(node *root)
-{
-    if(root != NULL)
-    {
-        cout << root->data <<"->";
-        preorder(root->left);
-        preorder(root->right);
-    }
+        if (balance < -1 && getbalance(root->right) > 0)
+        {
+            root ->right = rightrotation(root->right);
+            return leftrotation(root);
+        }
+    return root;
 }
-   
+    void preorder(node * root)
+    {
+        if (root != NULL)
+        {
+            cout << root->data << "->";
+            preorder(root->left);
+            preorder(root->right);
+        }
+    }
 
     int main()
     {
         // cout<<"preorder";
-        node*root=NULL;
+        node *root = NULL;
         root = insert(root, 10);
         root = insert(root, 5);
         root = insert(root, 2);
-        cout<<"preorder"<<endl;
+        cout << "preorder" << endl;
         preorder(root);
         return 0;
     }
